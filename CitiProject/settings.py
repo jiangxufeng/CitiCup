0cb42b279481e6354730fcc47a75c6e8cd0ee87d
szf <szfff@foxmail.com>
2018-09-10 19:53:58 +0800

    将数据库配置到服务器mysql

--- CitiProject/settings.py
+++ CitiProject/settings.py
@@ -82,8 +82,13 @@ WSGI_APPLICATION = 'CitiProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CITI',
+        'USER': 'citi',
+        'PASSWORD':'122885090a',
+        'HOST':'111.231.226.60',
+        'PORT':'3306',
+
     }
 }
 
